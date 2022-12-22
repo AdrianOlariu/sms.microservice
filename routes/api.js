2cce273bd799997f359bc73ce2ef758f03452ad7
root <adrian.olariu93@gmail.com>
2022-12-23 01:21:04 +0200

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+router.route('/').get((req, res)=>{
+	res.send('<h1>API</h1>');
+});
+
+module.exports = router;
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const sms = require('./routes/api.js');
+
+app.get('^/$|/index(.html)?',(req,res)=>{
+	res.sendFile(path.join(__dirname,'views','index.html'));
+	}
+);
+
+app.get('/json', (req,res)=>{
+	res.send({'json':'working'});
+});
+
+app.use('/sms', require('./routes/sms'));
+app.use('/api', require('./routes/api'));
+
+app.get('/*', (req,res)=>{
+	res.redirect(301, '/index');
+});
+
+app.listen(4000, () => {
+	console.log('server running');
+});
+
+console.log('sms');
