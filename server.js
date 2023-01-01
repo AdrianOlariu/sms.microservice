6ff0ce0d3adcd027c0970a5a5823f0f7ca6eb8d5
Adrian Olariu <adrian.olariu93@gmail.com>
2023-01-01 12:56:47 +0200

    test 5

--- server.js
+++ server.js
@@ -28,3 +28,4 @@ console.log('sms');
 //test web hook
 //test web hook (we use the port in the web hook because of the port forwarding and not using ddns for the router so we need to specify the port. the app port it is set on nginx reverse proxy)
 //test 4
+//test 5
