63a4955823e081d0ec1fb121ef6c98291031b778
Adrian Olariu <adrian.olariu93@gmail.com>
2023-01-17 13:21:32 +0200

    sms folders:outgoing sent failed and middleware more optimised

--- middleware/smsConfig.js
+++ middleware/smsConfig.js
@@ -2,34 +2,59 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 const {format} = require('date-fns');
-const { exit } = require('process');
 
 function generateName(){
     return `${format(new Date(),'dd-MM-yyyy-hh-mm-ss')}.txt`;
 }
 
-function createSms(phone, text){
-    if(phone[0] === '0'){
-        phone = '+4' + phone;
-    }else if(phone[0] === '4'){
-        phone = '+' + phone;
-    }else if(phone[0] === '+'){
-        phone = phone;
-    }
-    let smsFileName = `sms-${generateName()}`;
-    fs.writeFileSync(path.join(__dirname,'..', 'sms','outgoing', smsFileName), `To:${phone}\n\n${text}`);
-    //treat the cases in which a wrong phone number is inserted
-}
-
-function checkSmsStatus(smsFileName, res){
-    fs.watch(path.join(__dirname,'..', 'sms','sent'), (eventType, fileName) => {
-        console.log(eventType);
-        console.log('reached');
-        console.log(fileName);
-        })
-        if(smsFileName === fileName){
-            return res;
+function checkNumberFormat(){
+    return (req, res, next) =>{
+        let {phone} = req.body;
+        if(phone[0] != '+' && (phone[0] != '4') && phone[0] != '0' && phone[0] != '7'){
+            res.status(400);
+            res.json({"failed":"phone number format incorrect"});
+        }else{
+            next();
         }
+    }
 }
 
-module.exports = {createSms, checkSmsStatus};
+function createSms(){
+    return (req, res, next) => {
+        let {phone, text} = req.body;
+        if(phone[0] === '0'){
+            phone = '+4' + phone;
+        }else if(phone[0] === '4'){
+            if(phone[1] === '0'){
+                phone = '+' + phone;
+            }else{
+                res.status(400);
+                return res.json({"failed":"phone number format incorrect"});
+            }
+        }else if(phone[0] === '+'){
+            phone = phone;
+        }else if(phone[0] === '7'){
+            phone = '+40' + phone;
+        }
+        let smsFileName = `sms-${generateName()}`;
+        fs.writeFileSync(path.join(__dirname,'..', 'sms','outgoing', smsFileName), `To:${phone}\n\n${text}`);
+        checkSmsStatus(smsFileName, res, next);
+            
+    }
+}
+
+function checkSmsStatus(smsFileName, res, next){
+        fs.watch(path.join(__dirname,'..', 'sms','outgoing', smsFileName), (eventType, fileName) => {
+            console.log(eventType);
+            console.log('reached');
+            console.log(fileName);
+                if(fs.existsSync(path.join(__dirname,'..', 'sms','sent', smsFileName))){
+                    next();
+                }else if(fs.existsSync(path.join(__dirname,'..', 'sms','failed', smsFileName))){
+                    res.status(500).json({"failed":"sms not sent due to internal server error"});
+                }   
+            })
+            
+}
+
+module.exports = {checkNumberFormat, createSms, checkSmsStatus};
--- routes/sms.js
+++ routes/sms.js
@@ -2,19 +2,18 @@
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
-const {createSms, checkSmsStatus} = require('../middleware/smsConfig');
+const {createSms, checkNumberFormat, checkSmsStatus} = require('../middleware/smsConfig');
 
 router.get('/',(req,res)=>{
 
 	res.json({"ceva":"ceva"});
 });
 
-router.post('/',(req,res)=>{
+router.post('/',checkNumberFormat(), createSms(), (req,res)=>{
 	const {phone, text} = req.body;
 	console.log(phone, text);
-	createSms(phone,text);
-	
 	res.json({"success":"sms sent to Q","phone":phone, "text":text});
+	
 });
 
 
